707db431d05f3db1428fbf1451475d64578ae834
nawesan12 <nahueltecnicomdp@gmail.com>
2022-01-09 00:59:34 -0300

    start app

---
+++ components/Layout.jsx
@@ -0,0 +1,32 @@
+export default function Layout() {
+    return(
+        <>
+            <nav className="navigation">
+                <div className="link">
+
+                </div>
+                <div className="link">
+
+                </div>
+                <div className="link">
+
+                </div>
+                <div className="link">
+
+                </div>
+                <div className="link">
+
+                </div>
+            </nav>
+            <style jsx>{`
+                .navigation {
+                    position:fixed;
+                    bottom:0;
+                    width:100vw;
+                    height:12vh;
+                    background:#000;
+                }    
+            `}</style>
+        </>
+    )
+}
