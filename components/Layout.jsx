10504517251456ef8b900f71a0fa1e10f7f28bef
nawesan12 <nahueltecnicomdp@gmail.com>
2022-01-09 01:06:34 -0300

    asd

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,21 +1,22 @@
-export default function Layout() {
+export default function Layout({ children }) {
     return(
         <>
+            {children}
             <nav className="navigation">
                 <div className="link">
-
+                    hola
                 </div>
                 <div className="link">
-
+hola
                 </div>
                 <div className="link">
-
+hola
                 </div>
                 <div className="link">
-
+hola
                 </div>
                 <div className="link">
-
+hola
                 </div>
             </nav>
             <style jsx>{`
@@ -24,7 +25,12 @@ export default function Layout() {
                     bottom:0;
                     width:100vw;
                     height:12vh;
-                    background:#000;
+                    background:#292929;
+                    display:flex;
+                    justify-content:space-around;
+                    align-items:center;
+                    border-top:3px solid #ccc;
+                    color:white;
                 }    
             `}</style>
         </>
---
+++ components/NanuTok.jsx
@@ -0,0 +1,12 @@
+export default function NanuTok() {
+    return(
+        <>
+            <h1>Aca va un tiktok uwu</h1>
+            <style jsx>{`
+                h1 {
+                    margin:auto;
+                }
+            `}</style>
+        </>
+    )
+}
