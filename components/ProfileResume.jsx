8d946bcaf1088ac9b0c8193dd25fee021d06ab07
nawesan12 <nahueltecnicomdp@gmail.com>
2022-01-10 01:08:13 -0300

    userrrrrr

--- components/ProfileResume.jsx
+++ components/ProfileResume.jsx
@@ -1,37 +1,49 @@
-export default function ProfileResume(props) {
+import ProfileVideos from './ProfileVideos';
+
+export default function ProfileResume({user}) {
     return(
         <>
         <header className="profile_settings">
-            <span><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <div>
+            <span><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-user-plus" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <circle cx="12" cy="7" r="4" />
-            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+            <circle cx="9" cy="7" r="4" />
+            <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+            <path d="M16 11h6m-3 -3v6" />
             </svg></span>
-            <h4>@{`username`} <span>▼</span></h4>
-            <span><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-dots-vertical" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
-            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-            <circle cx="12" cy="12" r="1" />
-            <circle cx="12" cy="19" r="1" />
-            <circle cx="12" cy="5" r="1" />
-            </svg></span>
+            </div>
+            
+            <h4>@{user.username} <span>▼</span></h4>
+
+            <div>
+                <span><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-menu-2" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                <line x1="4" y1="6" x2="20" y2="6" />
+                <line x1="4" y1="12" x2="20" y2="12" />
+                <line x1="4" y1="18" x2="20" y2="18" />
+                </svg></span>
+            </div>
         </header>
         <section className="profile_info">
-            <div className="profile_picture">
-
+            <div className="user_info">
+            <div className="profile_picture"></div>
+            <span className="profile_username">@{user.username}</span>
             </div>
-            <span className="profile_username">@{`username`}</span>
+            
             
             <div className="profile_statistics">
                 <div className="item">
-                    <p>50</p>
+                    <p>{user.statistics.following.toString()}</p>
                     <span>Following</span>
                 </div>
+                <span className="separation">|</span>
                 <div className="item">
-                    <p>800</p>
+                    <p>{user.statistics.followers.toString()}</p>
                     <span>Followers</span>
                 </div>
+                <span className="separation">|</span>
                 <div className="item">
-                    <p>3k</p>
+                    <p>{user.statistics.likes.toString()}</p>
                     <span>Likes</span>
                 </div>
             </div>
@@ -49,12 +61,11 @@ export default function ProfileResume(props) {
                 <path d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z" />
                 </svg></span>
             </div>
+
+            <p>{user.description}</p>
         </section>
-        <section className="profile_videos">
-            <header className="videos_header">
-                videos header
-            </header>
-        </section>
+
+        <ProfileVideos />
 
         <style jsx>{`
 
@@ -70,24 +81,36 @@ export default function ProfileResume(props) {
                 font-size:.7rem;
             }
 
+            .profile_settings div span {
+                margin-left:.5rem;
+            }
+
             .profile_settings {
                 height:8vh;
             }
 
             .profile_info {
-                height:38vh;
+                height:40vh;
                 width:100vw;
                 display:flex;
                 flex-direction:column;
                 justify-content:space-evenly;
                 align-items:center;
+                padding-bottom:.5rem;
+                border-bottom: 1px solid #ccc;
             }    
+
+            .user_info {
+                display:flex;
+                flex-direction:column;
+                align-items:center;
+            }
 
             .profile_picture {
                 height:5rem;
                 width:5rem;
                 border-radius:50%;
-                background:url('https://picsum.photos/100/100');
+                background:url(${user.profilePicture});
             }
 
             .profile_username {
@@ -103,10 +126,15 @@ export default function ProfileResume(props) {
 
             .item {
                 margin:0 1rem;
+                width:17vw;
             }
 
             .item p {
                 font-weight:bold;
+            }
+
+            .item span {
+                font-size:.9rem;
             }
 
             .profile_options {
@@ -128,6 +156,13 @@ export default function ProfileResume(props) {
 
             .profile_options .edit {
                 padding:.4rem 1.2rem;
+            }
+
+            .separation {
+                font-weight:200;
+                color:#ccc;
+                margin:.5rem 0;
+                font-size:.8rem;
             }
         `}</style>       
 
---
+++ components/ProfileVideos.jsx
@@ -0,0 +1,83 @@
+import { useState } from 'react'
+
+export default function ProfileVideos() {
+
+    const [activePersonalVideos, setActivePersonalVideos] = useState(false)
+    const [activeLikedVideos, setActiveLikedVideos] = useState(false)
+    const [activePrivateVideos, setActivePrivateVideos] = useState(false)
+     
+    const openPersonalVideos = () => {
+        setActivePersonalVideos(true)
+        setActiveLikedVideos(false)
+        setActivePrivateVideos(false)
+    }
+
+    const openLikedVideos = () => {
+        setActivePersonalVideos(false)
+        setActiveLikedVideos(true)
+        setActivePrivateVideos(false)
+    }
+
+    const openPrivateVideos = () => {
+        setActivePersonalVideos(false)
+        setActiveLikedVideos(false)
+        setActivePrivateVideos(true)
+    }
+
+    return(
+        <>
+        <header>
+            <div><button onClick={openPersonalVideos} className={activePersonalVideos === true ? 'button_active' : ''} ><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-layout-grid" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <rect x="4" y="4" width="6" height="6" rx="1" />
+            <rect x="14" y="4" width="6" height="6" rx="1" />
+            <rect x="4" y="14" width="6" height="6" rx="1" />
+            <rect x="14" y="14" width="6" height="6" rx="1" />
+            </svg></button></div>
+            <div><button onClick={openLikedVideos} className={activeLikedVideos === true ? 'button_active' : ''}><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-heart" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+            </svg></button></div>
+            <div><button onClick={openPrivateVideos} className={activePrivateVideos === true ? 'button_active' : ''}><svg xmlns="http://www.w3.org/2000/svg" className="icon icon-tabler icon-tabler-lock" width="24" height="24" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#000000" fill="none" strokeLinecap="round" strokeLinejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <rect x="5" y="11" width="14" height="10" rx="2" />
+            <circle cx="12" cy="16" r="1" />
+            <path d="M8 11v-4a4 4 0 0 1 8 0v4" />
+            </svg></button></div>
+        </header>
+        <section className="profile_videos">
+            {/*<PersonalVideos display={activePersonalVideos === true ? true : false} />
+            <LikedVideos display={activeLikedVideos === true ?  true : false}/>
+            <PrivateVideos display={activePrivateVideos === true ? true : false}/>*/}
+        </section>
+
+        <style jsx>{`
+            header {
+                width:100vw;
+                height:6vh;
+                border-bottom:1px solid #ccc;
+                display:flex;
+                justify-content:space-around;
+                align-items:center;
+            }    
+
+            header div {
+                display:flex;
+                align-items:center;
+            }
+
+            button {
+                height:6vh;
+                border:none;
+                outline:none;
+                background:none;
+                border-bottom:2px solid transparent;
+            }
+
+            .button_active {
+                border-bottom:2px solid #000;
+            }
+        `}</style>
+        </>
+    )
+}
