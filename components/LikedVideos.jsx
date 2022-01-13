1fae594bb78f4b0136dbc3483ba70eb69640587a
nawesan12 <nahueltecnicomdp@gmail.com>
2022-01-13 01:16:01 -0300

    asd

--- components/LikedVideos.jsx
+++ components/LikedVideos.jsx
@@ -1,3 +1,24 @@
 export default function LikedVideos({display, videos}) {
-    {if(display === true) {return (<p>Liked Videos</p> )} else { return (<></>)}}
+    if (display) {
+        return (
+        <div className="liked-videos" style={{marginBottom:'8vh', display:'flex', flexDirection:'row', flexWrap:'wrap'}}>
+            {videos.map((video, index) => (
+                <div className="liked-video-container" key={index} style={{width:'33vw'}} >
+                    <iframe
+                        style={{background:'#ddd', width:'32vw', height:'55vw'}}	
+                        src={video.url}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                    />
+                    <div className="liked-video-info" style={{float:'right', position:'relative', bottom:'3.5vh', right:'1.5vh'}}>
+                        <p>{video.views}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+        );
+    } else {
+        return null;
+    }
 }
--- components/PersonalVideos.jsx
+++ components/PersonalVideos.jsx
@@ -1,3 +1,25 @@
 export default function PersonalVideos({display, videos}) {
-    {if(display === true) {return (<p>Personal Videos</p> )} else { return (<></>)}}
+    if (display) {
+        return (
+        <div className="personal-videos" style={{marginBottom:'8vh', display:'flex', flexDirection:'row', flexWrap:'wrap'}}>
+            {videos.map((video, index) => (
+                <div className="personal-video-container" key={index} style={{width:'33vw'}} >
+                    <iframe
+                        style={{background:'#ddd', width:'32vw', height:'55vw'}}	
+                        src={video.url}
+                        frameBorder="0"
+                        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        thumbnail={video.thumbnail}
+                    />
+                    <div className="personal-video-info" style={{float:'right', position:'relative', bottom:'3.5vh', right:'1.5vh'}}>
+                        <p>{video.views}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+        );
+    } else {
+        return null;
+    }
 }
