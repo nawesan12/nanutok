bb260e63f5676d3193de98e45e37b6b723b94725
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2022-01-13 18:09:05 -0300

    Create ChatBox.jsx

---
+++ components/ChatBox.jsx
@@ -0,0 +1,12 @@
+export default function ChatBox({lastMessage, user}) {
+    return(
+        <>
+        <article className="chat_box">
+        
+        </article>
+        <style jsx>{`
+
+        `}</style>
+        </>
+    )
+}
